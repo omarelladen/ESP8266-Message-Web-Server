1623e10718d6ea66ac5cb773de0c978b8a48de32
Omar Zagonel El Laden <omarzladen@gmail.com>
2026-02-16 21:39:53 -0300

    mv defines to a separate file

---
+++ defines.h
@@ -0,0 +1,28 @@
+#define GPIO0 0
+#define GPIO1 1
+#define GPIO2 2
+#define GPIO3 3
+
+#define MAX_NUM_MSGS 50
+#define MAX_LEN_BODY 1024
+#define MAX_LEN_MSG  512
+#define MAX_LEN_REQ  50
+
+#define HTML_START "<!DOCTYPE HTML><html>"
+#define HTML_END   "</html>"
+#define HTML_BODY_STYLE "style='font-family: Arial; text-align: center;'"
+
+#define MSG_FIELD "message"
+
+#define EP_SEND      "/form"
+#define EP_BASE_SEND "/send/browser"
+#define EP_SEND_TEXT "/send/browser/text"
+#define EP_SEND_URL  "/send/browser/url"
+#define EP_SEND_CLI  "/send/cli"
+#define EP_LOOK      "/look"
+#define EP_CLEAR     "/clear"
+
+#define HW_NAME "ESP8266-01"
+
+#define SSID ""
+#define PASSWORD ""
--- server.ino
+++ server.ino
@@ -1,33 +1,5 @@
 #include <ESP8266WiFi.h>
-
-#define GPIO0 0
-#define GPIO1 1
-#define GPIO2 2
-#define GPIO3 3
-
-#define MAX_NUM_MSGS 50
-#define MAX_LEN_BODY 1024
-#define MAX_LEN_MSG  512
-#define MAX_LEN_REQ  50
-
-#define HTML_START "<!DOCTYPE HTML><html>"
-#define HTML_END   "</html>"
-#define HTML_BODY_STYLE "style='font-family: Arial; text-align: center;'"
-
-#define MSG_FIELD "message"
-
-#define EP_SEND      "/form"
-#define EP_BASE_SEND "/send/browser"
-#define EP_SEND_TEXT "/send/browser/text"
-#define EP_SEND_URL  "/send/browser/url"
-#define EP_SEND_CLI  "/send/cli"
-#define EP_LOOK      "/look"
-#define EP_CLEAR     "/clear"
-
-#define HW_NAME "ESP8266-01"
-
-#define SSID ""
-#define PASSWORD ""
+#include "defines.h"
 
 
 WiFiServer g_server(80);  // arg is the port to listen on
