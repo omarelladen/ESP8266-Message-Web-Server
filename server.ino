bfec2fd2eb473bfd4b5e0088633521222960e449
Omar Zagonel El Laden <omarzladen@gmail.com>
2026-02-21 16:22:47 -0300

    use utf-8

--- server.ino
+++ server.ino
@@ -17,7 +17,7 @@ void sendHeader(WiFiClient* p_client)
 {
     p_client->print(F(
         "HTTP/1.1 200 OK\r\n"
-        "Content-Type: text/html\r\n\r\n"
+        "Content-Type: text/html; charset=UTF-8\r\n\r\n"
     ));
 }
 
